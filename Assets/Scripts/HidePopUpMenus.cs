8167a24bdc1903d1acbd1f367d6803981f72ed32
kesh1092 <qtip1092@gmail.com>
2017-09-30 22:17:42 -0600

    Added 'New Game' Submenu after selecting New Game in the main menu. (just hit New Game in the main menu to view.)

---
+++ Assets/Scripts/HidePopUpMenus.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+//Prevent all canvas' that show after clicking menu buttons 
+public class HidePopUpMenus : MonoBehaviour
+{
+	public Transform newGameMenu;
+  
+	void Start()
+	{
+		newGameMenu.gameObject.SetActive(false);
+	}
+
+}
---
+++ Assets/Scripts/ShowSubMenusScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Show specific sub-menu after clicking menu button options
+/// </summary>
+public class ShowSubMenusScript : MonoBehaviour
+{
+	public Transform newGameMenu;
+
+
+	public void ShowNewGameMenu()
+	{
+		newGameMenu.gameObject.SetActive(true);
+	}	
+
+}
