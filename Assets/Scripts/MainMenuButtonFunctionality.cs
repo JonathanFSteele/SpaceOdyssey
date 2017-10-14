c8ff788c8c0738539ea0b67be1017e965d64039b
Jonathan Frank Steele <jonathan@steeleconsult.com>
2017-10-14 14:59:03 -0600

    Implemented quit application functionality...

---
+++ Assets/Scripts/MainMenuButtonFunctionality.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtonFunctionality : MonoBehaviour {
+
+	public Transform ExitGameMenu;
+
+	public void ShowExitGame()
+	{
+		//Show "Are you sure?" Popup "Yes" or "No"
+		ExitGameMenu.gameObject.SetActive(true);
+	}
+
+	public void ExitGameChoice(bool value)
+	{
+		//if Yes
+		if (value == true) {
+			Debug.Log("Quitting Game...");
+			Application.Quit ();
+		} else {
+			ExitGameMenu.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/ShowAndHideUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Is a class that will hold the show and hiding of menus (Excluding the Encounter Menu Popups)
+public class ShowAndHideUI : MonoBehaviour
+{
+	public Transform newGameMenu;
+	public Transform loadGameMenu;
+	// public Transform saveGameMenu; //will assign later
+
+	public void ShowNewGameMenuTF(bool value)
+	{
+		newGameMenu.gameObject.SetActive(value);
+	}	
+
+	public void ShowLoadGameMenuTF(bool value)
+	{
+		loadGameMenu.gameObject.SetActive(value);
+	}
+
+}
