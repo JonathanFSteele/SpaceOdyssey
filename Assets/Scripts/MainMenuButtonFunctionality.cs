bbc061db5cbae568c3ab745da1644a7e34481a61
Jonathan Frank Steele <jonathan@steeleconsult.com>
2017-10-14 16:26:51 -0600

    Fixed UI, to properly render on the right layer...

--- Assets/Scripts/MainMenuButtonFunctionality.cs
+++ Assets/Scripts/MainMenuButtonFunctionality.cs
@@ -1,8 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButtonFunctionality : MonoBehaviour {
+
+	public Transform OverwritePopup;
+	private int CurrentSelectedSlot;
+
+	public void overwriteGamePopup(int saveSpaceNumber)
+	{
+		//Show "Are you sure?" Popup "Yes" or "No"
+		CurrentSelectedSlot = saveSpaceNumber;
+		OverwritePopup.gameObject.SetActive(true);
+	}
+
+	public void NewGame(bool value)
+	{
+		Debug.Log ("Are you allowing a new Game?: " + value);
+		if (value == true) {
+			Debug.Log ("Starting New Game on Game Slot: " + CurrentSelectedSlot);
+			//NewGame Functionality
+			SceneManager.LoadScene ("Safezone");
+		} else {
+			//Cancel Save
+			OverwritePopup.gameObject.SetActive(false);
+		}
+	}
+
+	public void LoadGame()
+	{
+		Debug.Log ("Loading Game on Game Slot: " + CurrentSelectedSlot);
+		//Load Functionality
+		SceneManager.LoadScene ("Safezone");
+	}
+		
+	// *****************************************************
+	// Exit Game Functionality Below
+	// *****************************************************
 
 	public Transform ExitGameMenu;
 
