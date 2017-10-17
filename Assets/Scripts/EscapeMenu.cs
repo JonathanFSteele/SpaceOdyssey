733cea71a1934f8c1b4b8b7ba2b21d7636d81b17
kesh1092 <qtip1092@gmail.com>
2017-10-16 20:57:27 -0600

    preliminary version of Ship canvas on Safezone scene

--- Assets/Scripts/EscapeMenu.cs
+++ Assets/Scripts/EscapeMenu.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class EscapeMenu : MonoBehaviour {
 
-	public Transform canvas;
+	public Transform canvas;	
+
+	public Transform hideSafeZoneUIcanvas;
 
 	void Update () {
+
 		if (Input.GetKeyDown(KeyCode.Escape)) {
+
+			if (hideSafeZoneUIcanvas.gameObject.activeSelf == true)
+				hideSafeZoneUIcanvas.gameObject.SetActive(false);
 
 			if (canvas.gameObject.activeInHierarchy == false){
 				canvas.gameObject.SetActive(true);
