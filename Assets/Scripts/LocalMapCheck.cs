e18e1c8eb4a90fa1b0d0c0ce23a576e0076a35a9
Zachary Morgan <zacp635@nmsu.edu>
2017-10-29 20:17:46 -0600

    Created the ImageLibrary object under global objects and the image library scripts for Captains, Crew, Item, and Ship.

--- Assets/Scripts/LocalMapCheck.cs
+++ Assets/Scripts/LocalMapCheck.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class LocalMapCheck : MonoBehaviour {
 
+	public GameObject localMapButton;
 	// Use this for initialization
 	void Start () {
 		
