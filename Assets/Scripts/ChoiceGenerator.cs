37010bc93156b68bc2a6a5f1bc170b22ed3a7d58
Jonathan Frank Steele <jonathan@steeleconsult.com>
2017-11-12 14:39:55 -0700

    Implemented Choice Generator

---
+++ Assets/Scripts/ChoiceGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceGenerator : MonoBehaviour {
+
+	private GameObject CurrentPlayer;
+	public int CurrentPosition;
+
+	// Use this for initialization
+	void Start () {
+		CurrentPlayer = GameObject.Find("Player");
+		CurrentPosition = CurrentPlayer.GetComponent<Player> ().CurrentLocationID;
+		Debug.Log("CurrentPosition: " + CurrentPosition);
+
+		if (CurrentPosition == 1) { //Earth
+			Debug.Log("You are on the Earth");
+
+
+
+		} else if (CurrentPosition == 2) { //Moon
+			Debug.Log("You are on the Moon");
+
+
+
+		}
+	}
+}
