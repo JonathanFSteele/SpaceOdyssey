6ef7bcf81fcd8385f80bb2461dba63c613ee1591
Luis A <qtip1092@gmail.com>
2017-11-12 17:24:27 -0700

    Added separate scene to implement shipyard called 'ShipYard'.  Will incorporate it later.  (ShipYard is incomplete)

--- Assets/Scripts/Libraries/ShipLibrary.cs
+++ Assets/Scripts/Libraries/ShipLibrary.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ShipLibrary : MonoBehaviour {
 
 	public ImageGroup[] imageGroups;
+	public Sprite displayImage;
+
+	public int scrollIndex = 0;
+	public int shipArraySize;
 
 	Dictionary<string, Sprite[]> groupDictionary = new Dictionary<string, Sprite[]>();
 
@@ -12,23 +16,71 @@ public class ShipLibrary : MonoBehaviour {
 		foreach (ImageGroup imageGroup in imageGroups) {
 			groupDictionary.Add (imageGroup.groupID, imageGroup.group);
 		}
+
+		Sprite[] sounds = groupDictionary ["shop_ships"];
+			
+		shipArraySize = sounds.Length;
+		print ("shipArraySize: " + shipArraySize);
 	}
 
-	public Sprite GetClipFromName(string name) {
+
+	public Sprite getNextShip(string name) {
 		if (groupDictionary.ContainsKey(name)) {
-			Sprite[] sounds = groupDictionary[name];
-			return sounds[0];
+
+			Sprite[] sounds = groupDictionary[name]; 
+
+			scrollIndex++;
+			if (scrollIndex >= shipArraySize)
+				scrollIndex = 0;
+
+			print ("scrollIndex: " + scrollIndex);
+
+			return sounds[scrollIndex];
 		}
+		print ("getNextShip() fail");
 		return null;
 	}
 
-	public Sprite GetRandomClipFromName(string name) {
+
+	public Sprite getPreviousShip(string name) {
 		if (groupDictionary.ContainsKey(name)) {
-			Sprite[] sounds = groupDictionary[name];
-			return sounds[Random.Range(0,sounds.Length)];
+
+			Sprite[] sounds = groupDictionary[name]; 
+
+			scrollIndex--;
+			if (scrollIndex <= 0)
+				scrollIndex = shipArraySize-1;
+
+			print ("scrollIndex: " + scrollIndex);
+
+			return sounds[scrollIndex];
 		}
+		print ("getPreviousShip() fail");
 		return null;
 	}
+
+
+
+
+
+//
+//	public Sprite GetClipFromName(string name) {
+//		if (groupDictionary.ContainsKey(name)) {
+//			Sprite[] sounds = groupDictionary[name]; 
+//			return sounds[0];
+//		}
+//		print ("GetClipFromName() fail");
+//		return null;
+//	}
+//
+//	public Sprite GetRandomClipFromName(string name) {
+//		if (groupDictionary.ContainsKey(name)) {
+//			Sprite[] sounds = groupDictionary[name];
+//			return sounds[Random.Range(0,sounds.Length)];
+//		}
+//		print ("GetRandomClipFromName() fail");
+//		return null;
+//	}
 
 
 	[System.Serializable]
