a6d52d13d3060996ea17b7e2f7e1d7c6ca8ebeb4
Luis A <qtip1092@gmail.com>
2017-11-16 23:47:14 -0700

    Added PrefabShipLibrary.  Shipyard scene can load prefab ships.  (not complete yet)

---
+++ Assets/Scripts/Libraries/PrefabShipLibrary.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabShipLibrary : MonoBehaviour {
+
+	// public GameObject test1;
+
+	public Ship_List[] shipList;
+	// public Sprite displayImage;
+
+	public int scrollIndex = 0;
+	public int shipArraySize;
+
+	Dictionary<string, Ship[]> groupDictionary = new Dictionary<string, Ship[]>();
+
+	void Awake() {
+		foreach (Ship_List foo in shipList) {
+			groupDictionary.Add (foo.groupID, foo.group);
+		}
+
+		Ship[] sounds = groupDictionary ["shop_ships"];
+			
+		shipArraySize = sounds.Length;
+		print ("shipArraySize: " + shipArraySize);
+	}
+
+
+	public Sprite getFirstShip(string name) {
+		if (groupDictionary.ContainsKey(name)) {
+
+			Ship[] sounds = groupDictionary[name]; 
+			return sounds[0].shipPicture;
+		}
+		print ("getFirstShip() fail");
+		return null;
+	}
+
+
+
+	public Sprite getNextShip(string name) {
+		if (groupDictionary.ContainsKey(name)) {
+
+			Ship[] sounds = groupDictionary[name]; 
+
+			scrollIndex++;
+			if (scrollIndex >= shipArraySize)
+				scrollIndex = 0;
+
+			print ("scrollIndex: " + scrollIndex);
+
+			return sounds[scrollIndex].shipPicture;
+		}
+		print ("getNextShip() fail");
+		return null;
+	}
+
+
+	public Sprite getPreviousShip(string name) {
+		if (groupDictionary.ContainsKey(name)) {
+
+			Ship[] sounds = groupDictionary[name]; 
+
+			scrollIndex--;
+			if (scrollIndex < 0)
+				scrollIndex = shipArraySize-1;
+
+			print ("scrollIndex: " + scrollIndex);
+
+			return sounds[scrollIndex].shipPicture;
+		}
+		print ("getPreviousShip() fail");
+		return null;
+	}
+
+
+
+ 
+	[System.Serializable]
+	public class Ship_List {
+		public string groupID;
+		public Ship[] group;
+	}
+}
---
+++ Assets/Scripts/PrefabShipLoad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrefabShipLoad : MonoBehaviour {
+
+//	public GameObject distanceText;
+//	public GameObject timeText;
+	public GameObject image;
+	public GameObject library;
+//	public GameObject player;
+
+
+	// Use this for initialization
+	public void Start () {
+//		player = GameObject.FindGameObjectWithTag ("Player");
+	    library = GameObject.FindGameObjectWithTag ("Library");
+//		player = GameObject.Find ("Player");
+//		library = GameObject.Find ("Library");
+	}
+
+
+	public void nextShopShip() {
+		library = GameObject.FindGameObjectWithTag ("Library");
+		//		print ("loadShip()");
+		image.GetComponent<UnityEngine.UI.Image>().sprite =  library.GetComponent<PrefabShipLibrary> ().getNextShip ("shop_ships");
+	}
+		
+	public void previousShopShip() {
+		library = GameObject.FindGameObjectWithTag ("Library");
+		//		print ("loadShip()");
+		image.GetComponent<UnityEngine.UI.Image>().sprite =  library.GetComponent<PrefabShipLibrary> ().getPreviousShip ("shop_ships");
+	}
+
+	public void loadShipYard() {
+		library = GameObject.FindGameObjectWithTag ("Library");
+		//		print ("loadShip()");
+		if(image.GetComponent<UnityEngine.UI.Image>().sprite == null)
+			image.GetComponent<UnityEngine.UI.Image>().sprite =  library.GetComponent<PrefabShipLibrary> ().getFirstShip ("shop_ships");
+	}
+
+	public void TransitToEncounter () {
+		SceneManager.LoadScene ("Encounter");
+	}
+}
