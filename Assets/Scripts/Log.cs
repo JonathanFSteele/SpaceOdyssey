ede737290b27601175bbc4803319edbfcf741330
Alec Gonzales <baldwin5513@gmail.com>
2017-11-16 17:43:02 -0700

    Added Log.cs and added more textfields in Captains log

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour 
+{
+    public Sprite CaptainImage;
+    public String CaptainName;
+    public String LogText;
+    public String mission;
+    public String Mod;
+    public int Mvalue;
+    public String trait1;
+    public String trait2;
+    public String trait3;
+    public int Tvalue1;
+    public int Tvalue2;
+    public int Tvalue3;
+    
+
+
+}
