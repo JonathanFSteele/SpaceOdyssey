cc5f68703e3bc76518318612d41343e16614b926
Luis A <qtip1092@gmail.com>
2017-11-25 12:10:48 -0700

    Added InitializeGame script to UIScript gameObject inside of MainMenuStartup.  Makes player start a new game with no ship, and 5000 credits.

---
+++ Assets/Scripts/InitializeGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeGame : MonoBehaviour {
+
+  public Player player;
+  public Ship playerShip;
+  public Ship noShip;
+  public int initialCredits;
+
+
+
+	void Start () {
+
+    player.credits = initialCredits;
+    // playerShip = noShip; DOESNT WORK this way
+
+    playerShip.shipPicture = noShip.shipPicture;
+    playerShip.shipName = noShip.shipName;
+    playerShip.maxHealth = noShip.maxHealth;
+    // if(playerShip.health > noShip.maxHealth)
+      playerShip.health = noShip.maxHealth;
+    playerShip.shields = noShip.shields;
+    playerShip.armor = noShip.armor;
+    playerShip.speed = noShip.speed;
+    playerShip.gunCount = noShip.gunCount;
+    playerShip.gunDamage = noShip.gunDamage;
+    playerShip.maxFuel = noShip.maxFuel;
+    playerShip.fuelEfficiency = noShip.fuelEfficiency;
+    // if(playerShip.fuel > noShip.maxFuel)
+      playerShip.fuel = noShip.maxFuel;
+    playerShip.crewCapacity = noShip.crewCapacity;
+    // if(playerShip.crewAmt > noShip.crewCapacity)
+      playerShip.crewAmt = noShip.crewCapacity;
+    playerShip.maxSupplies = noShip.maxSupplies;
+    // if(playerShip.supplies > noShip.maxSupplies)
+      playerShip.supplies = noShip.maxSupplies;
+    // playerShip.medicalBonus = noShip.medicalBonus;
+    // playerShip.combatBonus = noShip.combatBonus;
+    // playerShip.charsmaBonus = noShip.charsmaBonus;    
+		
+	}
+	
+ 
+}
