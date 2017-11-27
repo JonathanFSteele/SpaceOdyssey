64f504069f293f323588afd153d5198fec5a54a7
Zachary Morgan <zacp635@nmsu.edu>
2017-11-26 18:55:47 -0700

    WOrking on stuff steele gave me

---
+++ Assets/Scripts/MissionControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionControl : MonoBehaviour {
+
+	public GameObject M1Name;
+	public GameObject M1Reward;
+	public GameObject M1Start;
+	public GameObject M1End;
+	public GameObject M1Description;
+
+	public GameObject M2Name;
+	public GameObject M2Reward;
+	public GameObject M2Start;
+	public GameObject M2End;
+	public GameObject M2Description;
+
+	public GameObject M3Name;
+	public GameObject M3Reward;
+	public GameObject M3Start;
+	public GameObject M3End;
+	public GameObject M3Description;
+
+	public Mission mission1;
+	public Mission mission2;
+	public Mission mission3;
+
+
+	public void UpdateUI(){
+
+	}
+
+	public void GetMission1(){
+
+
+	}
+
+	public void GetMission2(){
+
+
+	}
+
+	public void GetMission3(){
+
+
+	}
+
+}
